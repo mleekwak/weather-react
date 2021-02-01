4174c14adb6c178c45c3af4463448e50dfebd95f
mleekwak <77120184+mleekwak@users.noreply.github.com>
2021-02-01 14:50:44 -0700

    new React app for weather final project

---
+++ src/Search.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchEngine() {
+  let [city, setCity] = useState("");
+  let [temperature, setTemperature] = useState("");
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [icon, setIcon] = useState("");
+
+  function showTemperature(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=0a5979c7e58e2739c3bb2a745f8efe0d`;
+    axios.get(url).then(showTemperature);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="Form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for a city"
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+
+      <ul>
+        <li>Temperature: {temperature}°F</li>
+        <li>Description: {description}</li>
+        <li>Humidity: {humidity}%</li>
+        <li>Wind: {wind} km/h</li>
+        <li>
+          <img src={icon} alt="weather" />
+        </li>
+      </ul>
+    </div>
+  );
+}
